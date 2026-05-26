0f54c61d7dba04977e330b5a55412f142bafea80
Paulo <paulogsbrito@gmail.com>
2026-05-26 15:08:33 -0300

    Feat: atualiza repositorio de funcionario e propriedades do projeto

--- src/main/java/com/generation/projetorh/repository/FuncionarioRepository.java
+++ src/main/java/com/generation/projetorh/repository/FuncionarioRepository.java
@@ -2,6 +2,8 @@ package com.generation.projetorh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.generation.projetorh.model.Funcionario;
+
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 
 }
