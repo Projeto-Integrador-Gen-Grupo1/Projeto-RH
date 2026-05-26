e0e37c272a22fc2aad861aa69e1c09d02bf64c64
Felipe-MDB <femarques.brito@outlook.com>
2026-05-26 13:45:52 -0300

    Adicionando interface

---
+++ src/main/java/com/generation/projetorh/repository/PostagemRepository.java
@@ -0,0 +1,7 @@
+package com.generation.projetorh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+}
